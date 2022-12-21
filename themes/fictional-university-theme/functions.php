23c75e043a2baae79f7fa8e87d42133053c799e2
kieran-calavan <93680051+kieran-calavan@users.noreply.github.com>
2022-12-21 13:40:40 -0600

    custom background image

--- themes/fictional-university-theme/functions.php
+++ themes/fictional-university-theme/functions.php
@@ -15,9 +15,7 @@ function university_features() {
   add_theme_support('post-thumbnails');
   add_image_size('professorLandscape', 400, 260, true);
   add_image_size('professorPortrait', 480, 650, true);
-  // register_nav_menu('headerMenuLocation', 'Header Menu Location');
-  // register_nav_menu('footerLocationOne', 'Footer Location One');
-  // register_nav_menu('footerLocationTwo', 'Footer Location Two');
+  add_image_size('pageBanner', 1500, 350, true);
 }
 
 add_action('after_setup_theme', 'university_features');
