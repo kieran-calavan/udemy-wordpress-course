d1c2f6b8214e8288f207b7870f59c5b40b59734c
kieran-calavan <93680051+kieran-calavan@users.noreply.github.com>
2022-12-23 15:42:11 -0600

    custom post type search layout

--- themes/fictional-university-theme/build/index.asset.php
+++ themes/fictional-university-theme/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery'), 'version' => '43a3fa62b24fce6f3345');
+<?php return array('dependencies' => array('jquery'), 'version' => '1958c17c833e5dca43f0');
--- themes/fictional-university-theme/src/modules/search.js
+++ themes/fictional-university-theme/src/modules/search.js
@@ -36,29 +36,70 @@ class Search {
 				}
 				this.typingTimer = setTimeout(this.getResults.bind(this), 750);
 			}
-			} else {
-				this.resultsDiv.html('');
-				this.isSpinnerVisible = false;
-			}
+		} else {
+			this.resultsDiv.html('');
+			this.isSpinnerVisible = false;
+		}
 
 		this.previousValue = this.searchField.val();
 	}
 
 	getResults() {
-		$.when(
-			$.getJSON(universityData.root_url + '/wp-json/wp/v2/posts?search=' + this.searchField.val()),
-			$.getJSON(universityData.root_url + '/wp-json/wp/v2/pages?search=' + this.searchField.val())
-			).then((posts, pages) => {
-			var combinedResults = posts[0].concat(pages[0]);
+		$.getJSON(universityData.root_url + '/wp-json/university/v1/search?term=' + this.searchField.val(), (results) => {
 			this.resultsDiv.html(`
-			<h2 class="search-overlay__section-title">General Information</h2>
-			${combinedResults.length ? '<ul class="link-list min-list">' : '<p>No general information matches that search</p>'}
-				${combinedResults.map(item => `<li><a href="${item.link}">${item.title.rendered}</a>${item.type == 'post' ? ` by ${item.authorName}` : ''}</li>`).join('')}
-				${combinedResults.length ? '</ul>' : ''}
-		`);
-		this.isSpinnerVisible = false;
-		},() => {
-			this.resultsDiv.html('<p>Unexpected Error; please try again.</p>')
+				<div class="row">
+					<div class="one-third">
+						<h2 class="search-overlay__section-title">General Information</h2>
+						${results.generalInfo.length ? '<ul class="link-list min-list">' : '<p>No general information matches that search</p>'}
+						${results.generalInfo.map(item => `<li><a href="${item.link}">${item.title}</a>${item.postType == 'post' ? ` by ${item.authorName}` : ''}</li>`).join('')}
+						${results.generalInfo.length ? '</ul>' : ''}
+					</div>
+
+					<div class="one-third">
+						<h2 class="search-overlay__section-title">Programs</h2>
+						${results.programs.length ? '<ul class="link-list min-list">' : `<p>No program(s) matches that search. <a href="${universityData.root_url}/programs">View all programs</a></p>`}
+						${results.programs.map(item => `<li><a href="${item.link}">${item.title}</a></li>`).join('')}
+						${results.programs.length ? '</ul>' : ''}
+						<h2 class="search-overlay__section-title">Professors</h2>
+						${results.professors.length ? '<ul class="professor-cards">' : `<p>No professors match that search.</p>`}
+						${results.professors.map(item => `
+							<li class="professor-card__list-item">
+							<a class="professor-card" href="${item.link}">
+							<img class="professor-card__image" src="${item.image}" alt="">
+							<span class="professor-card__name">${item.title}</span>
+							</a>
+							</li>
+						`).join('')}
+						${results.professors.length ? '</ul>' : ''}
+
+					</div>
+
+					<div class="one-third">
+						<h2 class="search-overlay__section-title">Campuses</h2>
+						${results.campuses.length ? '<ul class="link-list min-list">' : `<p>No campuses match that search. <a href="${universityData.root_url}/campuses">View all campuses</a></p>`}
+						${results.campuses.map(item => `<li><a href="${item.link}">${item.title}</a></li>`).join('')}
+						${results.campuses.length ? '</ul>' : ''}
+
+						<h2 class="search-overlay__section-title">Events</h2>
+						${results.events.length ? '' : `<p>No events match that search. <a href="${universityData.root_url}/events">View all events</a></p>`}
+						${results.events.map(item => `
+						<div class="event-summary">
+						<a class="event-summary__date t-center" href="${item.link}">
+							<span class="event-summary__month">${item.month}</span>
+							<span class="event-summary__day">${item.day}</span>
+						</a>
+						<div class="event-summary__content">
+							<h5 class="event-summary__title headline headline--tiny"><a href="${item.link}">${item.title}</a></h5>
+							<p>${item.description}<a href="${item.link}" class="nu gray">Learn more</a></p>
+				</div>
+					</div>
+						`).join('')}
+
+					</div>
+
+				</div>
+			`);
+			this.isSpinnerVisible = false;
 		});
 	}
 
@@ -76,7 +117,7 @@ class Search {
 		this.searchOverlay.addClass('search-overlay--active');
 		$("body").addClass("body-no-scroll");
 		this.searchField.val('');
-		setTimeout( () => this.searchField.focus(), 301);
+		setTimeout(() => this.searchField.focus(), 301);
 		this.isOverlayOpen = true;
 	}
 
