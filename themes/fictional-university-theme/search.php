2547bc70f04ac784caab6592740a102e849d163c
kieran-calavan <93680051+kieran-calavan@users.noreply.github.com>
2023-01-03 15:37:28 -0600

    formatting

--- themes/fictional-university-theme/search.php
+++ themes/fictional-university-theme/search.php
@@ -5,12 +5,12 @@ pageBanner(array(
   'title' => 'Search Results',
   'subtitle' => 'You searched for &ldquo;' . esc_html(get_search_query(false))  . '&rdquo;'
 ));
- ?>
+?>
 
 <div class="container container--narrow page-section">
-<?php
+  <?php
   if (have_posts()) {
-    while(have_posts()) {
+    while (have_posts()) {
       the_post();
       get_template_part('template-parts/content', get_post_type());
     }
@@ -20,8 +20,8 @@ pageBanner(array(
   }
 
   get_search_form();
-  
-?>
+
+  ?>
 
 </div>
 
