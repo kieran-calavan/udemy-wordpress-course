b4c6edcb826e6124760961138f69bd0773dd2430
kieran-calavan <93680051+kieran-calavan@users.noreply.github.com>
2023-01-13 11:07:14 -0600

    delete frontend data

--- plugins/new-database-table/new-database-table.php
+++ plugins/new-database-table/new-database-table.php
@@ -20,8 +20,22 @@ class PetAdoptionTablePlugin {
     // add_action('admin_head', array($this, 'onAdminRefresh'));
     add_action('admin_post_createpet', array($this, 'createPet'));
     add_action('admin_post_nopriv_createpet', array($this, 'createPet'));
+    add_action('admin_post_deletepet', array($this, 'deletePet'));
+    add_action('admin_post_nopriv_deletepet', array($this, 'deletePet'));
     add_action('wp_enqueue_scripts', array($this, 'loadAssets'));
     add_filter('template_include', array($this, 'loadTemplate'), 99);
+  }
+
+  function deletePet() {
+    if (current_user_can('administrator')) {
+      $id = sanitize_text_field($_POST['idtodelete']);
+      global $wpdb;
+      $wpdb->delete($this->tablename, array('id' => $id));
+      wp_safe_redirect(site_url('/pet-adoption'));
+    } else {
+      wp_redirect(site_url());
+    }
+    exit;
   }
 
   function createPet() {
@@ -30,10 +44,11 @@ class PetAdoptionTablePlugin {
       $pet['petname'] = sanitize_text_field($_POST['incomingpetname']);
       global $wpdb;
       $wpdb->insert($this->tablename, $pet);
-      wp_redirect(site_url('/pet-adoption'));
+      wp_safe_redirect(site_url('/pet-adoption'));
     } else {
       wp_redirect(site_url());
     }
+    exit;
   }
 
   function onActivate() {
