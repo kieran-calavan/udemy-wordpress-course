9777d6b5b9d049f27abc597fb9a3668bf4ccf21c
kieran-calavan <93680051+kieran-calavan@users.noreply.github.com>
2023-01-13 16:28:38 -0600

    theme.json

--- themes/fictional-block-theme/build/genericheading.asset.php
+++ themes/fictional-block-theme/build/genericheading.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blocks', 'wp-element'), 'version' => '3365adb740f2bcadc041');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element'), 'version' => '39c8c57f7c61ac3dd4ec');
