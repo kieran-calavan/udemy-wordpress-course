99356aa3eda7e92da5bba2ef20682f023843001c
kieran-calavan <93680051+kieran-calavan@users.noreply.github.com>
2023-01-13 15:08:01 -0600

    blocks and functions

---
+++ themes/fictional-block-theme/build/banner.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-element'), 'version' => '7f32a0e53b716f650f03');
---
+++ themes/fictional-block-theme/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('jquery'), 'version' => 'a100b113e675b9cdf182');
